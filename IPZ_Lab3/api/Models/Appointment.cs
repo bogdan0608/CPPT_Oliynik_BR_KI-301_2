843050b17d111c5bb81114e3f1d28df90abed470
Rikatoi <76889813+bogdan0608@users.noreply.github.com>
2025-12-20 08:14:11 +0200

    Add files via upload

---
+++ IPZ_Lab3/api/Models/Appointment.cs
@@ -0,0 +1,10 @@
+﻿namespace lab3_19.api.Models;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Time { get; set; }
+    public string Phone { get; set; }
+    public int ServiceId { get; set; } 
+}
---
+++ IPZ_Lab3/api/Models/UserScore.cs
@@ -0,0 +1,8 @@
+﻿namespace lab3_19.api.Models;
+
+public class UserScore
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Score { get; set; }
+}
